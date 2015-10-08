8a4fd67cb0f9832d0d24dab0972497129e1dc4aa
nick-benoit14 <nick.benoit14@gmail.com>
2015-10-07 19:54:49 -0600

    finished updateDataSet()

--- src/MakeData.js
+++ src/MakeData.js
@@ -1,16 +1,20 @@
 //---------------------current
-      //working on updatePointArr()
+      //working on draw();
+          //manageRangeList
+          //manageUpdates - resize pointArr & rawData
 
 //---------------------TODO
       //Build Tests - MakeDataSpec.js
-          //test updatePointArr
           //test updateDataset()
-          
+          //test manageUpdates()
+          //test manageRangeList()
+
 
           //range changed flag? if update new Max and new Min if no change to range
-
+          //if range = 0; plot straight line
 //---------------------updates
      //take displayNum as argument to controller
+     //added newMax and newMin. updated in updateRangeList min and max set to newMax and newMin in draw
 //---------------------Notes
 
 /*
@@ -31,6 +35,7 @@ function Controller(displayNum, height, width) //takes number of data points to 
       var tolerance = 100; //margin on sides of viewing window
       height -= tolerance;
       width -= tolerance;
+    
 
       this.Data =
         {
@@ -65,12 +70,53 @@ function Controller(displayNum, height, width) //takes number of data points to 
           //draws data formatted for last graph state
       this.updateDataset = function()
           {
-            //updateRawData()
-            //updatePointArray()
-            //return PointArray;
+            updateRawData();
+            updatePointArray();
+            return this.Data.pointArray; //use with d3.update(); draw off screen
           }
 
-      this.Draw = function(){} //handles things that need to happen often
+      this.Draw = function()
+        {
+          //recalcules x
+          //recalculates y if necessary
+          //resizes pointArray
+          //resizes raw_Data
+          //manages rangeList
+
+          var redrawRangeflag = false;
+
+          if(this.Data.min != this.Data.newMin || this.Data.max != this.Data.newMax){redrawRangeflag = true;} //recalculate y if new data added outside of current range
+
+
+
+          if(this.Data.pointArray.length < this.Data.displayNum){return this.Data.pointArr;} //dont need to redraw any values
+          else
+            {
+
+              redrawRangeflag = this.manageLists(); //returns true if max or min changed
+
+
+
+              //need to redraw x values
+              //may need to redraw y values
+
+              for(var i = 0; i < this.Data.pointArr.length; i++) //recalculate values
+                  {
+                    if(redrawRangeflag)
+                      {
+                        //this.Data.pointArr[i].Y =
+                      }
+
+                     //this.Data.pointArr[i].X =
+
+                  }
+
+            }
+
+          //return pointArr
+
+        }
+          //handles things that need to happen often
                     //updates dynamic variables
                           //calculates x and y for newMax and newMin and then replaces
                           //max and min with new values. newMax and NewMin set in updateRangeList
@@ -80,7 +126,17 @@ function Controller(displayNum, height, width) //takes number of data points to 
 
 
 /*------------------Controller Methods --------------------------*/
-
+      this.clearData = function()
+        {
+          this.Data.pointArray = [];
+          this.Data.rawData = [];
+          this.Data.rangeList = [];
+          this.Data.dataBuffer = [];
+          this.Data.max = undefined;
+          this.Data.min = undefined;
+          this.Data.newMax = undefined;
+          this.Data.newMin = undefined;
+        }
       this.binaryInsert = function(arr, val, a, b) //inserts val into ordered array
         {
           if(b < 0) b = 0;
@@ -97,6 +153,22 @@ function Controller(displayNum, height, width) //takes number of data points to 
           if(arr[m] < val) this.binaryInsert(arr, val, m + 1, b);
           else this.binaryInsert(arr, val, a, m);
         }
+        this.binaryDelete = function(arr, val, a, b) //inserts val into ordered array
+          {
+            if(b < 0) b = 0;
+            var m = Math.floor((a + b) / 2);
+
+            if(a == b) //final value
+              {
+                var flag = false;
+                if(a == 0 || a == arr.length) flag = true; //if deleted from front or back of list return true
+                 if(arr[m] > val)arr.splice(m-1,1); //add to front of arr (arr[0])
+                 else arr.splice(m,1); //add after arr[m]
+                 return flag;
+              }
+            if(arr[m] < val) this.binaryDelete(arr, val, m + 1, b);
+            else this.binaryDelete(arr, val, a, m);
+          }
 
       this.updateRangeList = function(inY) //takes array of raw Y values. adds it to sorted rangeList, update max and min
         {
@@ -120,8 +192,16 @@ function Controller(displayNum, height, width) //takes number of data points to 
             if(typeof this.Data.min == 'undefined') this.Data.min = min;
         }
 
-
-
+      this.manageRangeList = function(clippings)
+        {
+          var flag = false;
+          for(var i = 0; i < clippings.length; i++)
+            {
+                flag = this.binaryDelete(this.Data.pointArray, clippings[i].Y);
+                //if deleted max or min reset max and min
+            }
+          return flag;
+        }
 
       this.updateRawData = function() //takes data from dataBuffer and pushes it to rawData
         {
@@ -149,35 +229,47 @@ function Controller(displayNum, height, width) //takes number of data points to 
 
           var val_X;
           var val_Y;
+          var range;
+
+            if(this.Data.max == this.Data.min){range = 1;}
+            else{range = this.Data.max - this.Data.min;}
 
           for(var i = this.Data.pointArray.length, j = 0; j < this.Data.rawData.length; i++, j++)
             {
               val_X = ((this.Data.displayWidth / this.Data.displayNum) * i) + (this.Data.tolerance / 2);
-              val_Y =   ((this.Data.rawData[j] - this.Data.min) * this.Data.displayHeight / (this.Data.max - this.Data.min))
+              val_Y =   ((this.Data.rawData[j] - this.Data.min) * this.Data.displayHeight / range)
               + this.Data.tolerance / 2;
-
               this.Data.pointArray.push({"X":val_X,"Y":val_Y});
             }
-
           return this.Data.pointArray;
-
-
         }
 
+     this.manageLists = function()
+      {
+        var clip = this.Data.pointArr - this.Data.displayNum;
 
 
-//      function Point(){} //stores x and y value to be graphed
+        this.Data.pointArray.slice(0,clip); //resize pointArray
+        var clippings = this.Data.rawData.slice(0,clip); //resize raw_Data
+
+        rangeFlag = this.manageRangeList(clippings); //returns true if max or min changed
+
+        return rangeFlag;
+
+      }
+
 
     this.test = function()
       {
-        var data = [1,2,4,21,4,2,3,2,-1,2,-6,10];
+        var data = [1,2,3,4,5];
         this.addData(data);
 
         this.updateRawData();
         this.updatePointArray();
+        console.log("height: " + this.Data.displayHeight);
 
-        this.addData(data);
-        this.updateRawData();
-        this.updatePointArray();
+        //this.addData(data);
+        //this.updateRawData();
+        //this.updatePointArray();
       }
 }
